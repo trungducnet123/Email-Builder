6df03730b0992fa0337aeb38f92dccb824afb3ff
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2022-10-21 15:33:23 +0700

    Update _config.php

--- server/_config.php
+++ server/_config.php
@@ -10,21 +10,21 @@
  */
 
 
-$protocol = 'http';
+$protocol = 'https';
 
 $host= $protocol . '://' . $_SERVER['SERVER_NAME'];
 
 $CONFIG['uploads_folder'] = 'server/uploads';
 
 $CONFIG['host'] = $host;
-$CONFIG['smtp']['host'] = '';
-$CONFIG['smtp']['username'] = '';
-$CONFIG['smtp']['password'] = '';
+$CONFIG['smtp']['host'] = 'smtp-relay.sendinblue.com';
+$CONFIG['smtp']['username'] = 'about@mediaz.vn';
+$CONFIG['smtp']['password'] = 'zSbZFnNfXB69QH7Y';
 $CONFIG['smtp']['secure'] = 'tls';
 $CONFIG['smtp']['port'] = 587;
 $CONFIG['smtp']['auth'] = true;
 
-$CONFIG['mail']['subject'] = '';
-$CONFIG['mail']['from_email'] = '';
-$CONFIG['mail']['from_name'] = '';
+$CONFIG['mail']['subject'] = '[MediaZ] Đầy là email test từ Email Builder';
+$CONFIG['mail']['from_email'] = 'no-reply@mediaz.vn';
+$CONFIG['mail']['from_name'] = 'MediaZ Support';
 $CONFIG['mail']['alt_body'] = 'The plain text for non-HTML mail clients';
